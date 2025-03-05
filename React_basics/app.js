a4621ba029fe1d243f7bf606350d836f3f0940b2
Saad_Naseem <raosad786@gmail.com>
2025-03-05 07:03:34 +0500

    Add basic React app with multiple heading elements

---
+++ React_basics/app.js
@@ -0,0 +1,21 @@
+
+
+
+const main_container = React.createElement('div', '{class:"main-div"}', [
+
+    React.createElement('h1' , '{id:"1"}', 'Hello world from H1'),
+
+    React.createElement('h2', '{id:"2"}', 'Hello world from H2'),
+
+    React.createElement('h3', '{id:"3"}', 'Hello world from H3'),
+
+    React.createElement('h4', '{id:"4"}', 'Hello world from H4'),
+
+    React.createElement('h5', '{id:"5"}', 'Hello world from H5'),
+
+    React.createElement('h6', '{id:"6"}', 'Hello world from H6')
+] )
+
+const root = ReactDOM.createRoot(document.getElementById('main'))
+
+root.render(main_container)
