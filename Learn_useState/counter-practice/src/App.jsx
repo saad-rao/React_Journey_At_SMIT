5d2ae5931f3f606a814af55500d2dc6bfadb8ecb
Saad_Naseem <raosad786@gmail.com>
2025-04-15 16:23:54 +0500

    implement useState for state management

--- Learn_useState/counter-practice/src/App.jsx
+++ Learn_useState/counter-practice/src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
 
-import './App.css'
+import './index.css'
 
-let count = 0  //Ya aik global variable hai jo ke har function ke liye accessible hai
+// let count = 0 
+ //Ya aik global variable hai jo ke har function ke liye accessible hai
 
 const App = () => {
 
 //is me hum na 2 functions banaye hain inc aur dec jo ke count ko increment aur decrement karte hain
 
 //abhi sirf ya function console ma print kar raha hai
-//abhi hum na useState ka use nahi kiya hai
+//abhi hum na useState ka use nahi kiyax hai
+const [count, setCount] = useState(0)
+//ab hum na useState ka use kiya hai jo ke count ko track karega
+// jab bhi count change hoga to component re-render hoga
 
   function inc (){
-    count+=1
+    setCount(count + 1)
     console.log("INC",count);
     
   }
 
   function dec (){
-    count-=1
+    // ab hum na useState ka use kiya hai jo ke count ko track karega
+    // jab bhi count change hoga to component re-render hoga
+
+    if(count === 0){
+      alert("Count can't be less than 0")
+      return
+    }
+    setCount(count - 1)
     console.log("DEC", count);
     
   }
+
+  // ab hum usestrate ka use karenge
+  // useState ka use karne se humara component re-render hoga jab bhi state change hogi
+  //aur jo 0 UI pa change nhi ho rha tha usa change karwae ga useState se
+ 
   return (
     <div>
-      <div>
-        <h1>Counter App</h1>
+      <div className='flex justify-center items-center h-screen bg-amber-500'>
+        <h1 className=''> Counter App</h1>
         
-        <button onClick={inc} className='p-1 rounded-sm bg-blue-300'>+</button>
+        <button onClick={inc}  className='px-3 mx-3 font-normal text-4xl py-2  rounded-sm bg-blue-300'>+</button>
         {count}
-        <button onClick={dec} className='p-1 rounded-sm ' >-</button>
+        <button onClick={dec}  className='px-3 mx-3 font-normal text-4xl py-2  rounded-sm bg-red-600 ' >-</button>
       </div>
     </div>
   )
